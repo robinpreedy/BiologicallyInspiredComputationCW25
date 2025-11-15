99a08b4196315d91383fa3a52b8d40be1e457bee
robinpreedy <rp2052@hw.ac.uk>
2025-11-15 17:44:10 +0000

    output for task1

--- scripts/run_EVAL.py
+++ scripts/run_EVAL.py
@@ -3,3 +3,4 @@ from scripts.task4_train__EVAL import train_ANN
 train_ANN("Concrete_Data_Yeh.csv")
 
 #python task4_train__EVAL.py --csv Concrete_Data_Yeh.csv
+#python task1_ANN.py --csv Concrete_Data_Yeh.csv
